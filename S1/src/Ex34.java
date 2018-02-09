45ba68f952c0638f50a05985c915a1675110d17c
Vinlagrenouille <vincent.navales@hotmail.fr>
2018-02-09 17:01:24 +0100

    not finished ex34

--- S1/src/Ex34.java
+++ S1/src/Ex34.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
 
 public class Ex34 {
 
-	public Ex34() {
-		// TODO Auto-generated constructor stub
+	public static void discard(String stc) {
+		char prev = '.';
+		char ances = '.';
+		for (char c : stc.toCharArray()) {
+			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+				System.out.print(c);
+			}
+			else {
+				System.out.println("");
+			}
+			
+		}
 	}
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String stc = sc.nextLine();
+		
+		discard(stc);
 	}
 
 }
