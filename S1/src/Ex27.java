253ccee3fdb46deff2b40ab2b5f3c6d5ac5a9b29
Vinlagrenouille <vincent.navales@hotmail.fr>
2018-02-09 16:23:10 +0100

    Ex27 done

---
+++ S1/src/Ex27.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex27 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+
+		int[] tab = new int[3];
+		int total = 0;
+		
+		for(int i = 0; i < 3; i++) {
+			tab[i] = sc.nextInt();
+			total = total + tab[i];
+		}
+		System.out.println(name);
+		System.out.println(total/3);
+	}
+
+}
---
+++ S1/src/Ex34.java
@@ -0,0 +1,13 @@
+
+public class Ex34 {
+
+	public Ex34() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
